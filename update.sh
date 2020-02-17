b5453977ce5e48d383afea0c98247e28b2b320e9
siddharthchouradiya <60422240+siddharthchouradiya@users.noreply.github.com>
2020-02-17 14:44:20 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,37 +1,26 @@
 #!/bin/bash
-
 set -u
-
 ##set user password
 user=qloapps
-
 echo -e "$USER_PASSWORD\n$USER_PASSWORD" | passwd $user
-echo "User qloapps password updated"
-
-##set mysql password
-
-sleep 4
+##Check for database connectivity
 database_connectivity_check=no
 var=1
 while [ "$database_connectivity_check" != "mysql" ]; do
 /etc/init.d/mysql start
-sleep 2
 database_connectivity_check=`mysqlshow --user=root | grep -o mysql`
-if [ $var -ge 4 ]; then
+if [ $var -ge 2 ]; then
 exit 1
 fi
 var=$((var+1))
 done
- 
+##Check for database
 database_availability_check=`mysqlshow --user=root | grep -ow "$MYSQL_DATABASE"`
- 
 if [ "$database_availability_check" == "$MYSQL_DATABASE" ]; then
 exit 1
 else
 mysqladmin -u root password $MYSQL_ROOT_PASSWORD
-mysql -u root -p$MYSQL_ROOT_PASSWORD -e "grant all on *.* to 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD';"
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "create database $MYSQL_DATABASE;"
-mysql -u root -p$MYSQL_ROOT_PASSWORD -e "grant all on $MYSQL_DATABASE.* to 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD';"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$MYSQL_ROOT_PASSWORD';"
 supervisorctl stop update_credentials && supervisorctl remove update_credentials
-echo "Database $MYSQL_DATABASE created"
 fi
